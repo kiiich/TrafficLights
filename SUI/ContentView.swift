b15317ac40891570dcaa03d22ebf34a610ff3936
kiiich <59602632+kiiich@users.noreply.github.com>
2021-10-26 18:24:10 +0300

    Initial Commit

---
+++ SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SUI
+//
+//  Created by Николай on 26.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Николай on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
